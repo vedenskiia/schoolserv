a754aecad4a9ef886a74f4b9c7252bc1eedbae05
Vedenskii A.S <vedenskiia@gmail.com>
2017-05-05 11:58:03 +0700

    Доблено резервное копирование файла настроек squid

--- install.sh
+++ install.sh
@@ -7,10 +7,17 @@ apt install squid -y
 cp /etc/samba/smb.conf{,.bak}
 useradd teacher -m
 passwd teacher
-useradd admin sudo -m
+usermod -G adminFTP -a admin
+useradd admin -m
 passwd admin
+sudo usermod -G userFTP -a teacher
 smbpasswd -a teacher
 smbpasswd -e teacher
 smbpasswd -a admin
 smbpasswd -e admin
-
+service smbd restart
+==============squid3============
+cp /etc/squid3/squid.conf /etc/squid3/squid.conf.original
+chmod a-w /etc/squid3/squid.conf.original
+service squid3 restart
+==============
